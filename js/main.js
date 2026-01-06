a96d7c27ae47d6e48637063eea40b6423b8dff6e
glubtubis-wepal <zbooker000@stu.waukesha.k12.wi.us>
2026-01-06 07:42:38 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,53 @@
-const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
+// main.js
+window.canvas = document.getElementById("game");
+window.ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-function loop() {
-  updatePhysics(canvas.width, canvas.height);
+const gravity = 0.5;
+const bounce = 0.8;
 
+function updatePhysics() {
+  for (let ball of objects) {
+    ball.vy += gravity;
+
+    ball.x += ball.vx;
+    ball.y += ball.vy;
+
+    // Floor
+    if (ball.y + ball.r > canvas.height) {
+      ball.y = canvas.height - ball.r;
+      ball.vy *= -bounce;
+    }
+
+    // Walls
+    if (ball.x - ball.r < 0 || ball.x + ball.r > canvas.width) {
+      ball.vx *= -bounce;
+    }
+  }
+}
+
+function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  for (let obj of objects) {
+  for (let ball of objects) {
     ctx.beginPath();
-    ctx.arc(obj.x, obj.y, obj.r, 0, Math.PI * 2);
+    ctx.arc(ball.x, ball.y, ball.r, 0, Math.PI * 2);
     ctx.fillStyle = "cyan";
     ctx.fill();
   }
+}
 
+function loop() {
+  updatePhysics();
+  draw();
   requestAnimationFrame(loop);
 }
 
 loop();
+
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
